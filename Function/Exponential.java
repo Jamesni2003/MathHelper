f0ff17a425ce30d2e3b352d09f79f651d122ba09
Jamesni <2667151484@qq.com>
2020-04-25 13:36:49 -0400

    question format

--- Function/Exponential.java
+++ Function/Exponential.java
@@ -4,27 +4,25 @@ public class Exponential extends Function {
 private double a;
 private double h;
 private double k;
-private double x;
-private double [] answer;
+private double y;
     public Exponential(){
-        answer = new double[1];
         a = (int) (Math.random() * 25 + 1);
         h = (int) (Math.random() * 25 + 1);
         k = (int) (Math.random() * 25 + 1);
-        x= (int) (Math.random() * 25 + 1);
-        answer[0] = ((double) Math.round(((Math.log10(x-k))/(Math.log10(a)) + h)*100))/100;
+        y = (int) (Math.random() * 25 + 1);
     }
     @Override
     public double[] X() {
-        System.out.println(answer[0]);
+        double [] answer = {((double) Math.round(((Math.log10(y-k))/(Math.log10(a)) + h)*100))/100};
         return answer;
     }
 
     @Override
     public String Question() {
-        
-        return "If there is an Exponential Equation " + a + "^" + "(x" +"-" + h + ")" + "+" + k + ". Solve for x if y is " + x + "??" + " If there is no solution put 0. (Round to the nearest hundreth)";
+        String a = (this.a > 0)? (int) this.a + "" : "-" + (int) -this.a; 
+        String h = (this.h == 0)? "" : (this.h > 0)? "+ " + (int) this.h : "- " + (int) -this.h;
+        String k = (this.k == 0)? "" : (this.k > 0)? "+ " + (int) this.k : "- " + (int) -this.k;
+        return (int)y + " = " + a + "^" + "(x " + h + ") " + k + ". Solve for x?(round answer to hundreth)";
     }
-
 
 }
